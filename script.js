cc14bb78e16fde6ecf176e3ab7b601831623fed8
Maria Lapshina <mariaspiiish@gmail,com>
2022-03-17 15:24:30 +0400

    feat: настроить работу кнопки submit

--- script.js
+++ script.js
@@ -26,3 +26,18 @@ document.addEventListener('keyup', function(event) {
         closePopup();
     }
 });
+
+let formElement = document.querySelector('.form');
+let nameInput = formElement.querySelector('.popup__text_type_name');
+let infoInput = formElement.querySelector('.popup__text_type_info');
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    const profileTitle = document.querySelector('.profile__title');
+    const profileSubtitle = document.querySelector('.profile__subtitle');
+    profileTitle.textContent = `${nameInput.value}`;
+    profileSubtitle.textContent = `${infoInput.value}`;
+    closePopup();
+}
+
+formElement.addEventListener('submit', formSubmitHandler); 
