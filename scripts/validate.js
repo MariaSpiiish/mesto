b12b134f857d4eb193f4b7d1b1d2b0dd9ac170ba
Maria Lapshina <mariaspiiish@gmail,com>
2022-04-13 15:02:09 +0400

    feat: функция enable validation принимает объект с классами и селекторами элементов

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,27 +1,27 @@
 // функция показывает ошибку ввода
-const showInputError = (formElement, inputElement, errorMessage) => {
+const showInputError = (formElement, inputElement, errorMessage, obj) => {
     const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-    inputElement.classList.add('popup__input_type_error');
+    inputElement.classList.add(`${obj.inputErrorClass}`);
     // Заменим содержимое span с ошибкой на переданный параметр
     errorElement.textContent = errorMessage;
-    errorElement.classList.add('popup__input-error_active');
+    errorElement.classList.add(`${obj.errorClass}`);
   };
 
 //функция  прячет сообщение об ошибке, если ошибки нет
-const hideInputError = (formElement, inputElement) => {
+const hideInputError = (formElement, inputElement, obj) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove('popup__input_type_error');
-  errorElement.classList.remove('popup__input-error_active');
+  inputElement.classList.remove(`${obj.inputErrorClass}`);
+  errorElement.classList.remove(`${obj.errorClass}`);
   // Очистим ошибку
   errorElement.textContent = '';
 };
 
 //проверить валидно ли поле
-const isValid = (formElement, inputElement) => {
+const isValid = (formElement, inputElement, obj) => {
   if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
+    showInputError(formElement, inputElement, inputElement.validationMessage, obj);
   } else {
-    hideInputError(formElement, inputElement);
+    hideInputError(formElement, inputElement, obj);
   }
 };
 
@@ -31,99 +31,46 @@ const hasInvalidInput = (inputList) => {
   });
 };
 
-const toggleButtonState = (inputList, buttonElement) => {
+const toggleButtonState = (inputList, buttonElement, obj) => {
   if (hasInvalidInput(inputList)) {
-    buttonElement.classList.add('popup__submit-button_inactive');
+    buttonElement.classList.add(`${obj.inactiveButtonClass}`);
   } else {
-    buttonElement.classList.remove('popup__submit-button_inactive');
+    buttonElement.classList.remove(`${obj.inactiveButtonClass}`);
   }
 };
 
 //установить обработчик события инпут на все поля ввода
-const setEventListeners = (formElement) => {
-  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
-  const buttonElement = formElement.querySelector('.popup__submit-button');
-  toggleButtonState(inputList, buttonElement);
+const setEventListeners = (formElement, obj) => {
+  const inputList = Array.from(formElement.querySelectorAll(`${obj.inputSelector}`));
+  const buttonElement = formElement.querySelector(`${obj.submitButtonSelector}`);
+  toggleButtonState(inputList, buttonElement, obj);
 
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', () => {
-    isValid(formElement, inputElement);
-    toggleButtonState(inputList, buttonElement);
+    isValid(formElement, inputElement, obj);
+    toggleButtonState(inputList, buttonElement, obj);
     });
   });
 };  
 
 //перебрать все формы, чтобы всем добавить слушатель submit
-const enableValidation = () => {
-  const formList = Array.from(document.querySelectorAll('.form'));
+const enableValidation = (obj) => {
+  const formList = Array.from(document.querySelectorAll(`${obj.formSelector}`));
 
   formList.forEach((formElement) => {
     formElement.addEventListener('submit', (evt) => {
       evt.preventDefault();
     });
 
-    setEventListeners(formElement);
+    setEventListeners(formElement, obj);
   });
 };
 
-enableValidation();
-
-// enableValidation({
-//     formSelector: '.popup__form',
-//     inputSelector: '.popup__input',
-//     submitButtonSelector: '.popup__button',
-//     inactiveButtonClass: 'popup__button_disabled',
-//     inputErrorClass: 'popup__input_type_error',
-//     errorClass: 'popup__error_visible'
-//   }); 
-  
-
-
-// //выбрать формы
-// const editProfileForm = document.forms.profile;
-// const addPlaceForm = document.forms.place;
-
-
-// const checkInputValidity = (input) => {
-//     return input.checkValidity();
-// }
-
-// const validateInput = (input) => {
-//     const errorElement = input.parentNode.querySelector(`#${input.id}-error`);
-//     checkInputValidity(input);
-//     errorElement.textContent = input.validationMessage;
-// };
-
-// //функция обрабатывает инпут
-// const handleInput = (evt) => {
-//     const currentForm = evt.currentTarget;
-//     const input = evt.target;
-
-//     validateInput(input);
-// };
-
-// //функция обрабатывает нажатие кнопки сабмит
-// const handleSubmit = (evt) => {
-//     evt.preventDefault();
-//     const currentForm = evt.target;
-//     if (currentForm.chackValidity()) {
-//         currentForm.reset();
-//     }
-// };
-
-// //добавить слушатель нажатия кнопки сабмит
-// editProfileForm.addEventListener('submit', handleSubmit);
-// addPlaceForm.addEventListener('submit', handleSubmit);
-
-// editProfileForm.addEventListener('input', handleInput);
-// addPlaceForm.addEventListener('input', handleInput);
-
-
-// enableValidation({
-//     formSelector: '.popup__form',
-//     inputSelector: '.popup__input',
-//     submitButtonSelector: '.popup__button',
-//     inactiveButtonClass: 'popup__button_disabled',
-//     inputErrorClass: 'popup__input_type_error',
-//     errorClass: 'popup__error_visible'
-//   }); 
+enableValidation({
+    formSelector: '.form',
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__submit-button',
+    inactiveButtonClass: 'popup__submit-button_inactive',
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'popup__input-error_active'
+  });
